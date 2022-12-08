b7d2154ea05d23ef99cf8ca287f94e5b8881b197
Kofoworawealth <mojeedkofoworola2020@gmail.com>
2022-12-08 05:13:55 -0800

    py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list_as_unique_set = set(my_list)
-
-    result = 0
-
-    for num in my_list_as_unique_set:
-        result += num
-
-        return result
+    add = 0
+            for i in set(my_list):
+                        add += i
+                            return add
